865e125f0babee1c5088c30f46d5504ab864c1b7
innocence-mo <1251847142@qq.com>
2020-12-16 22:35:20 +0800

    添加登录注册路由

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="desc">Ant Design Vue Pro</div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-]
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: '/user',
+      component: () => import(/* webpackChunkName: "layout" */ '../layouts/UserLayout'),
+      children: [
+        {
+          path: '/user',
+          redirect: '/user/login',
+        },
+        {
+          path: '/user/login',
+          name: 'login',
+          component: () => import(/* webpackChunkName: "user" */ '../views/User/Login.vue'),
+        },
+        {
+          path: '/user/register',
+          name: 'register',
+          component: () => import(/* webpackChunkName: "user" */ '../views/User/Register.vue'),
+        },
+      ],
+    },
+  ],
 })
-
-export default router
